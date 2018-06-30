2a3295fc50dea4a82cf82646a282bb023b6f8fa4
Lou117 <lou117@users.noreply.github.com>
2018-06-30 18:08:26 +0200

    Adds composer.json Adds ErrorHandling middlewares

---
+++ src/ErrorHandlingJsonMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 14/06/2018
+ * Time: 20:54
+ */
+namespace Lou117\Cockade\Middleware;
+
+use \Throwable;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\Psr7\stream_for;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Basic implementation for a error handling middleware formatting any Throwable to a JSON response with HTTP code 500.
+ *
+ * PSR-15 specification: "It is RECOMMENDED that any application using middleware include a component that catches
+ * exceptions and converts them into responses. This middleware SHOULD be the first component executed and wrap all
+ * further processing to ensure that a response is always generated."
+ *
+ * Please note that this implementation will ALWAYS return a ready-to-use JSON response, which can be problematic if
+ * incoming HTTP request Accept header doesn't allow for "application/json" Content-Type (a.k.a. content negotiation).
+ * @package Lou117\Cockade\Middleware
+ */
+class ErrorHandlingJsonMiddleware implements MiddlewareInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+
+            return $handler->handle($request);
+
+        } catch (Throwable $e) {
+
+            $response = new Response();
+            return $response
+                ->withStatus(500)
+                ->withHeader("Content-Type", "application/json")
+                ->withBody(stream_for(json_encode([
+                    "error" => $e->getMessage(),
+                    "trace" => $e->getTrace()
+                ])));
+
+        }
+    }
+}
---
+++ src/ErrorHandlingTextMiddleware.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 14/06/2018
+ * Time: 20:54
+ */
+namespace Lou117\Cockade\Middleware;
+
+use \Throwable;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use function GuzzleHttp\Psr7\stream_for;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Basic implementation for a error handling middleware formatting any Throwable to a text response with HTTP code 500.
+ *
+ * PSR-15 specification: "It is RECOMMENDED that any application using middleware include a component that catches
+ * exceptions and converts them into responses. This middleware SHOULD be the first component executed and wrap all
+ * further processing to ensure that a response is always generated."
+ *
+ * Please note that this implementation will ALWAYS return a ready-to-use text response, which can be problematic if
+ * incoming HTTP request Accept header doesn't allow for "text/plain" Content-Type (a.k.a. content negotiation).
+ * @package Lou117\Cockade\Middleware
+ */
+class ErrorHandlingTextMiddleware implements MiddlewareInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+
+            return $handler->handle($request);
+
+        } catch (Throwable $e) {
+
+            $response = new Response();
+            return $response
+                ->withStatus(500)
+                ->withHeader("Content-Type", "text/plain")
+                ->withBody(stream_for((string) $e));
+
+        }
+    }
+}
